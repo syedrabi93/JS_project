baef24b8476c53cd9b6d80458dd82e77faac3b57
Adarshdeep Singh <adeee621@gmail.com>
2021-06-21 18:48:41 +0530

    Added Api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://lampton-api.app.zap-torrent.com",
     transformRequest: [
         ...axios.defaults.transformResponse,
         (req, headers) => {
