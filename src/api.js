c76ace2111050b3a35ba04a991da98f2375dfb0d
Adarshdeep Singh <adeee621@gmail.com>
2021-06-21 19:39:47 +0530

    Fixed Code

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
     baseURL: "https://lampton-api.app.zap-torrent.com",
     transformRequest: [
-        ...axios.defaults.transformResponse,
+        ...axios.defaults.transformRequest,
         (req, headers) => {
             const token = localStorage.getItem("AuthToken");
             if (token) {
