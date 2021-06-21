27279a718f7a589cbbdfe4356700fbd46cfdc905
Adarshdeep Singh <adeee621@gmail.com>
2021-06-21 22:05:56 +0530

    Fixed Booking Design

--- src/components/Pages/Book.js
+++ src/components/Pages/Book.js
@@ -75,104 +75,116 @@ function Book() {
     useEffect(() => {
         fetchDesigns();
     }, []);
+    if (currentDesign === null) {
+        return null;
+    }
 
     return (
         //image section
-        <div className="splitScreen">
-            <div className="topPane">
-                <div className="cards">
-                    <h1 class="h1_align">You Like this Design?</h1>
-                    <div className="cards__wrapper">
-                        <div className="book__item__img">
-                            {cardInfo.map(renderCard)}
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="cards">
+                        <h1 class="h1_align">You Like this Design?</h1>
+                        <h3>Name: {currentDesign.name}</h3>
+                        <h3>Price: ${currentDesign.price} CAD</h3>
+                        <div className="d-flex">
+                            {currentDesign.images.map((item) => {
+                                return (
+                                    <div style={{ width: 400, height: 400, padding: 20 }}>
+                                        <img
+                                            className="img-fluid position-relative"
+                                            src={item.url}
+                                        />
+                                    </div>
+                                );
+                            })}
                         </div>
                     </div>
                 </div>
-            </div>
-
-            {/*form section*/}
-            <div className="bottomPane">
-                <form onSubmit={bookHandler}>
-                    <div className="form-inner">
-                        <h2>Book the Event</h2>
-                        <div className="form-group">
-                            <label htmlFor="name">Your Name: </label>
-                            <input
-                                type="text"
-                                name="name"
-                                id="name"
-                                onChange={(e) =>
-                                    setDetails({
-                                        ...details,
-                                        name: e.target.value,
-                                    })
-                                }
-                                value={details.name}
-                            />
+                <div className="col-md-6">
+                    <form onSubmit={bookHandler}>
+                        <div className="form-inner">
+                            <h2>Book the Event</h2>
+                            <div className="form-group">
+                                <label htmlFor="name">Your Name: </label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    onChange={(e) =>
+                                        setDetails({
+                                            ...details,
+                                            name: e.target.value,
+                                        })
+                                    }
+                                    value={details.name}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="venue">Venue:</label>
+                                <input
+                                    type="text"
+                                    name="venue"
+                                    id="venue"
+                                    onChange={(e) =>
+                                        setDetails({
+                                            ...details,
+                                            venue: e.target.value,
+                                        })
+                                    }
+                                    value={details.venue}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="contact">Contact:</label>
+                                <input
+                                    type="text"
+                                    name="contact"
+                                    id="contact"
+                                    onChange={(e) =>
+                                        setDetails({
+                                            ...details,
+                                            contact: e.target.value,
+                                        })
+                                    }
+                                    value={details.contact}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="email">email:</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    onChange={(e) =>
+                                        setDetails({
+                                            ...details,
+                                            email: e.target.value,
+                                        })
+                                    }
+                                    value={details.email}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="name">Date:</label>
+                                <input
+                                    type="date"
+                                    name="date"
+                                    id="date"
+                                    onChange={(e) =>
+                                        setDetails({
+                                            ...details,
+                                            date: e.target.value,
+                                        })
+                                    }
+                                    value={details.date}
+                                />
+                            </div>
+                            <input type="submit" value="Book" />
                         </div>
-                        <div className="form-group">
-                            <label htmlFor="venue">Venue:</label>
-                            <input
-                                type="text"
-                                name="venue"
-                                id="venue"
-                                onChange={(e) =>
-                                    setDetails({
-                                        ...details,
-                                        venue: e.target.value,
-                                    })
-                                }
-                                value={details.venue}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="contact">Contact:</label>
-                            <input
-                                type="text"
-                                name="contact"
-                                id="contact"
-                                onChange={(e) =>
-                                    setDetails({
-                                        ...details,
-                                        contact: e.target.value,
-                                    })
-                                }
-                                value={details.contact}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="email">email:</label>
-                            <input
-                                type="email"
-                                name="email"
-                                id="email"
-                                onChange={(e) =>
-                                    setDetails({
-                                        ...details,
-                                        email: e.target.value,
-                                    })
-                                }
-                                value={details.email}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="name">Date:</label>
-                            <input
-                                type="date"
-                                name="date"
-                                id="date"
-                                onChange={(e) =>
-                                    setDetails({
-                                        ...details,
-                                        date: e.target.value,
-                                    })
-                                }
-                                value={details.date}
-                            />
-                        </div>
-                        <input type="submit" value="Book" />
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     );
